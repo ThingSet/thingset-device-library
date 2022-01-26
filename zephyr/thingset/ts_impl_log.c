0b5130d9033f06eb5a6da637cba5520b50ac8b30
Bobby Noelte <b0661n0e17e@gmail.com>
2022-01-26 08:00:25 +0100

    COM prepare - add ThingSet OSAL for logging
    Prepare for ThingSet Communication Context:
    - Add operating system abstraction for logging.
    - Add Native implementation of logging
    - Add Zephyr implementation of logging
    
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>

---
+++ zephyr/thingset/ts_impl_log.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021 Bobby Noelte.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* Setup logging environment for the ThingSet library */
+#define TS_IMPL_LOG_REGISTER
+
+#include "../../src/thingset_env.h"
+
